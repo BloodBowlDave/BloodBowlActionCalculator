a31fed9d0d7f201e6ff9013f8efc01a75c5b9bd6
bloodbowldave <bloodbowldave@gmail.com>
2022-04-12 11:52:06 +0100

    wip

--- ActionCalculator/CalculationBuilder.cs
+++ ActionCalculator/CalculationBuilder.cs
@@ -10,6 +10,7 @@ namespace ActionCalculator
     {
         private readonly IActionBuilder _actionBuilder;
         private readonly IPlayerParser _playerParser;
+        private List<PlayerAction> _playerActions;
 
         public CalculationBuilder(IActionBuilder actionBuilder, IPlayerParser playerParser)
         {
@@ -19,136 +20,117 @@ namespace ActionCalculator
 
         public Calculation Build(string calculation)
         {
-            var playerStrings = calculation.Split('(', ')')
-                .Where(x => !string.IsNullOrWhiteSpace(x));
+            _playerActions = new List<PlayerAction>();
 
-            var playerActions = new List<PlayerAction>();
-            var i = 0;
-
-            foreach (var playerString in playerStrings)
-            {
-                foreach (var playerAction in GetPlayerActions(playerString, new Player()))
-                {
-                    playerAction.Index = i;
-                    i++;
-                    playerActions.Add(playerAction);
-                }
-            }
-
-            return new Calculation(playerActions.ToArray());
+            BuildPlayerActions(calculation, new Player(), 0);
+            
+            return new Calculation(_playerActions.ToArray());
         }
 
-        private IEnumerable<PlayerAction> GetPlayerActions(string playerString, Player player, int depth = 0)
+        private void AddPlayerActions(IEnumerable<PlayerAction> playerActions)
         {
-            var indexOfOpeningBrace = playerString.IndexOf('{');
-            var indexOfOpeningSquareBracket = playerString.IndexOf('[');
-
-            if (indexOfOpeningBrace > -1 && (indexOfOpeningBrace < indexOfOpeningSquareBracket || indexOfOpeningSquareBracket == -1))
+            foreach (var playerAction in playerActions)
             {
-                var indexOfClosingBrace = playerString.LastIndexOf('}');
+                playerAction.Index = _playerActions.Count;
+                _playerActions.Add(playerAction);
+            }
+        }
 
-                if (indexOfClosingBrace == -1)
+        private void BuildPlayerActions(string calculation, Player player, int depth)
+        {
+            var (specialCharacter, index) = GetFirstSpecialCharacter(calculation);
+
+            switch (specialCharacter)
+            {
+                case ':':
+                    player = _playerParser.Parse(calculation[..index]);
+                    BuildPlayerActions(calculation[(index + 1)..], player, depth);
+                    break;
+                case '{':
+                    BuildPlayerActionsWithBranch(calculation, player, depth, index);
+                    break;
+                case '[':
+                    BuildPlayerActionsWithFork(calculation, player, depth, index);
+                    break;
+                case ';':
+                    BuildPlayerActionsForMultiplePlayers(calculation, player, depth, index);
+                    break;
+                default:
+                    BuildPlayerActionsForOnePlayer(calculation, player, depth);
+                    break;
+            }
+        }
+
+        private static Tuple<char?, int> GetFirstSpecialCharacter(string calculation)
+        {
+            for (var i = 0; i < calculation.Length; i++)
+            {
+                var character = calculation[i];
+                foreach (var specialCharacter in new List<char> {':', ';', '{', '[', '('}.Where(x => character == x))
                 {
-                    throw new Exception("No matching closing brace.");
-                }
-
-                var indexOfColon = playerString.LastIndexOf(':');
-                if (indexOfColon > indexOfClosingBrace)
-                {
-                    player = _playerParser.Parse(playerString[(indexOfColon + 1)..]);
-                }
-                
-                foreach (var playerAction in GetActions(playerString[..indexOfOpeningBrace]).Select(x => new PlayerAction(player, x, depth)))
-                {
-                    yield return playerAction;
-                }
-
-                var isFirstAction = true;
-                foreach (var playerAction in GetPlayerActions(playerString[(indexOfOpeningBrace + 1)..indexOfClosingBrace], player, depth + 1))
-                {
-                    if (isFirstAction)
-                    {
-                        playerAction.Action.RequiresNonCriticalFailure = true;
-                        isFirstAction = false;
-                    }
-
-                    yield return playerAction;
-                }
-
-                var endOfActionString = indexOfColon != -1 ? indexOfColon : playerString.Length;
-
-                foreach (var playerAction in GetActions(playerString[(indexOfClosingBrace + 1)..endOfActionString]).Select(x => new PlayerAction(player, x, depth)))
-                {
-                    yield return playerAction;
+                    return new Tuple<char?, int>(specialCharacter, i);
                 }
             }
-            else if (indexOfOpeningSquareBracket > -1)
+
+            return new Tuple<char?, int>(null, -1);
+        }
+
+        private void BuildPlayerActionsForOnePlayer(string calculation, Player player, int depth) =>
+            AddPlayerActions(GetActions(calculation).Select(x => new PlayerAction(player, x, depth)));
+        
+        private void BuildPlayerActionsForMultiplePlayers(string calculation, Player player, int depth, int indexOfPlayerEnd)
+        {
+            AddPlayerActions(GetActions(calculation[..indexOfPlayerEnd]).Select(x => new PlayerAction(player, x, depth)));
+            BuildPlayerActions(calculation[(indexOfPlayerEnd + 1)..], new Player(), depth);
+        }
+
+        private void BuildPlayerActionsWithFork(string calculation, Player player, int depth, int indexOfOpeningSquareBracket)
+        {
+            var indexOfClosingSquareBracket = calculation.LastIndexOf(']');
+
+            if (indexOfClosingSquareBracket == -1)
             {
-                var indexOfClosingSquareBracket = playerString.LastIndexOf(']');
-
-                if (indexOfClosingSquareBracket == -1)
-                {
-                    throw new Exception("No matching closing bracket.");
-                }
-
-                var indexOfColon = playerString.LastIndexOf(':');
-                if (indexOfColon > indexOfClosingSquareBracket)
-                {
-                    player = _playerParser.Parse(playerString[(indexOfColon + 1)..]);
-                }
-
-                foreach (var playerAction in GetActions(playerString[..indexOfOpeningSquareBracket]).Select(x => new PlayerAction(player, x, depth)))
-                {
-                    yield return playerAction;
-                }
-
-                var isFirstAction = true;
-                PlayerAction previousPlayerAction = null;
-
-                foreach (var playerAction in GetPlayerActions(playerString[(indexOfOpeningSquareBracket + 1)..indexOfClosingSquareBracket], player, depth + 1))
-                {
-                    if (previousPlayerAction != null)
-                    {
-                        yield return previousPlayerAction;
-                    }
-
-                    if (isFirstAction)
-                    {
-                        playerAction.Action.RequiresNonCriticalFailure = true;
-                        isFirstAction = false;
-                    }
-
-                    previousPlayerAction = playerAction;
-                }
-
-                if (previousPlayerAction != null)
-                {
-                    previousPlayerAction.Action.TerminatesCalculation = true;
-                    yield return previousPlayerAction;
-                }
-
-                var endOfActionString = indexOfColon != -1 && indexOfColon > indexOfClosingSquareBracket ? indexOfColon : playerString.Length;
-
-                foreach (var playerAction in GetActions(playerString[(indexOfClosingSquareBracket + 1)..endOfActionString])
-                             .Select(x => new PlayerAction(player, x, depth)))
-                {
-                    yield return playerAction;
-                }
+                throw new Exception("No matching closing bracket.");
             }
-            else
+
+            AddPlayerActions(GetActions(calculation[..indexOfOpeningSquareBracket]).Select(x => new PlayerAction(player, x, depth)));
+            
+            var calculationLength = _playerActions.Count;
+
+            BuildPlayerActions(calculation[(indexOfOpeningSquareBracket + 1)..indexOfClosingSquareBracket], player, depth + 1);
+
+            var lengthOfFork = _playerActions.Count - calculationLength;
+
+            if (lengthOfFork > 0)
             {
-                var playerSplit = playerString.Split(':');
-
-                if (playerSplit.Length > 1)
-                {
-                    player = _playerParser.Parse(playerSplit[1]);
-                }
-                
-                foreach (var playerAction in GetActions(playerSplit[0]).Select(x => new PlayerAction(player, x, depth)))
-                {
-                    yield return playerAction;
-                }
+                _playerActions[calculationLength].Action.RequiresNonCriticalFailure = true;
+                _playerActions[calculationLength + lengthOfFork - 1].Action.TerminatesCalculation = true;
             }
+
+            AddPlayerActions(GetActions(calculation[(indexOfClosingSquareBracket + 1)..]).Select(x => new PlayerAction(player, x, depth)));
+        }
+
+        private void BuildPlayerActionsWithBranch(string calculation, Player player, int depth, int indexOfOpeningBrace)
+        {
+            var indexOfClosingBrace = calculation.LastIndexOf('}');
+
+            if (indexOfClosingBrace == -1)
+            {
+                throw new Exception("No matching closing brace.");
+            }
+
+            AddPlayerActions(GetActions(calculation[..indexOfOpeningBrace]).Select(x => new PlayerAction(player, x, depth)));
+
+            var calculationLength = _playerActions.Count;
+
+            BuildPlayerActions(calculation[(indexOfOpeningBrace + 1)..indexOfClosingBrace], player, depth + 1);
+            
+            if (_playerActions.Count - calculationLength > 0)
+            {
+                _playerActions[calculationLength].Action.RequiresNonCriticalFailure = true;
+            }
+
+            AddPlayerActions(GetActions(calculation[(indexOfClosingBrace + 1)..]).Select(x => new PlayerAction(player, x, depth)));
         }
 
         private IEnumerable<Action> GetActions(string input)
@@ -170,7 +152,6 @@ namespace ActionCalculator
                 }
 
                 var action = _actionBuilder.Build(actionSplit[1]);
-
                 action.RequiresNonCriticalFailure = true;
 
                 yield return action;
