90610fc98aa67c745e22d40257df068853a55685
bloodbowldave <bloodbowldave@gmail.com>
2022-07-29 15:36:50 +0100

    wip

---
+++ ActionCalculator.Web/Client/Shared/AppBar.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ActionCalculator.Web.Client.Shared
+{
+    public partial class AppBar
+    {
+        private bool _isLightMode = true;
+
+        [Parameter]
+        public EventCallback OnSidebarToggled { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OnThemeToggled { get; set; }
+
+        private async Task ToggleTheme()
+        {
+            _isLightMode = !_isLightMode;
+
+            await OnThemeToggled.InvokeAsync(_isLightMode);
+        }
+        
+        private void ToggleSidebar()
+        {
+            OnSidebarToggled.InvokeAsync();
+        }
+    }
+}
